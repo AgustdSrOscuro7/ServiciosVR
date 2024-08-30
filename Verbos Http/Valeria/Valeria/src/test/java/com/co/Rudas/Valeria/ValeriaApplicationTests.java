7a2ca4dd83a07c2cdb130d5b030bc3e84c7b7bb5
AgustdSrOscuro7 <valekrudas1@gmail.com>
2024-08-29 21:11:01 -0500

    verbos http

---
+++ Verbos Http/Valeria/Valeria/src/test/java/com/co/Rudas/Valeria/ValeriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.Rudas.Valeria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValeriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
